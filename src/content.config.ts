c39e94781f9a63de722749e4902450e0a16148d4
joshuamURD <jmacmaster9@gmail.com>
2025-03-03 09:58:25 +0800

    Team (feat): Creating the initial team page

---
+++ src/content.config.ts
@@ -0,0 +1,21 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const team = defineCollection({ 
+    loader: file("src/content/team.json"),
+    schema: ({image}) => z.object({
+        name: z.string(),
+        role: z.string(),
+        image: image(), // This will be a key to reference the image
+        bio: z.string(),
+        linkedin: z.string().optional(),
+        twitter: z.string().optional(),
+    }),
+ });
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { team };
