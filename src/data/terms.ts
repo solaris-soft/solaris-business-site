0a2ee3d9565fe07a141cbbf2631e39f529de8ff9
joshuamURD <jmacmaster9@gmail.com>
2025-05-03 10:21:58 +0800

    Adding booking feature and updating terms

---
+++ src/data/terms.ts
@@ -0,0 +1,97 @@
+export const sections = [
+  {
+    title: "Acceptance of Terms",
+    content:
+      "By engaging Solaris Software, you agree to be bound by these terms and conditions. All services are subject to these terms and conditions. Acceptance may be constituted by the completion of a project, or the signing of a contract, or by continuing to engage or instruct Solaris Software after the terms and conditions have been provided.",
+  },
+  {
+    title: "Services Agreement",
+    content:
+      "SOLARIS provides web development, business automation, UI/UX design, branding, digital consulting, and MVP development services. All services are provided on an 'as is' basis, subject to the terms outlined in individual project agreements.",
+  },
+  {
+    title: "Intellectual Property",
+    content:
+      "Unless explicitly stated otherwise in a separate agreement, SOLARIS maintains ownership of all processes, techniques, and methodologies used in service delivery. Upon full payment, clients receive ownership rights to the final deliverables as specified in the project scope.",
+  },
+  {
+    title: "Project Delivery",
+    content:
+      "Project timelines and deliverables will be clearly outlined in individual statements of work. While we strive to meet all projected deadlines, timelines may be subject to change based on project complexity, client feedback cycles, and scope modifications. Clients are providing timely feedback, assets and access to resources to ensure the project is delivered on time and to the highest standard. Solaris Software is not responsible for any delays or issues arising from the client's failure to provide timely or accurate feedback, assets or resources.",
+  },
+  {
+    title: "Payment Terms",
+    content:
+      "Failure to satisfy agreed payment terms may result in prevailing industry interest rates to be apply to late payments and the company reserves the right to take any necessary legal action to recover any accrued debts, include the costs associated with any such action.",
+  },
+  {
+    title: "Deposit",
+    content:
+      "A deposit of 50% of the total project cost is required to secure the project. This deposit is non-refundable. The remaining 50% is due upon completion of the project.",
+  },
+  {
+    title: "Consultations",
+    content:
+      "Initial consultations are free of charge. Further consultations are charged and booked through Solaris Software's booking system. In person consultations are charged at a higher rate.",
+  },
+  {
+    title: "Confidentiality",
+    content:
+      "We maintain strict confidentiality regarding all client information and project details. This includes business processes, strategic plans, and any proprietary information shared during the course of our engagement.",
+  },
+  {
+    title: "Project Modifications",
+    content:
+      "Any changes to the agreed project scope may impact timeline and costs. All modifications must be approved in writing and may require revised agreements or additional statements of work.",
+  },
+  {
+    title: "Warranty",
+    content:
+      "No warranties or representations (implied or otherwise) except those specific provided in writing in an individual agreement.  Otherwise, the company does not provide any warranties that the services rendered will achieve any specific or desired outcome.",
+  },
+  {
+    title: "Liabilities",
+    content:
+      "Any party contracting with Solaris Software acknowledges and agrees that any potential claims against Solaris Software, or its partners or officers or employees, is strictly limited to the total amount that has been paid for the services the subject of the claim.",
+  },
+  {
+    title: "Governing Law",
+    content:
+      "This Agreement shall be governed by and construed in accordance with the laws of the State of Western Australia, without regard to its conflict of law provisions.",
+  },
+  {
+    title: "Dispute Resolution",
+    content:
+      "Any disputes arising from this Agreement shall be resolved through mediation prior to any legal action being taken.",
+  },
+  {
+    title: "Guarantees",
+    content:
+      "Solaris Software provides no guarantees that the services rendered will achieve any specific or desired outcome.  All services are provided on an 'as is' basis. No software is guaranteed to be free of bugs or errors. No software is guaranteed to be free of security vulnerabilities. No software is guaranteed to be free of performance issues. No software is guaranteed to be free of technical debt. No software is guaranteed to not have any issues arise or deteriorate.",
+  },
+  {
+    title: "Refund Policy",
+    content:
+      "Solaris Software provides no refunds for any services rendered.  All services are non-refundable.",
+  },
+  {
+    title: "SEO Guarantees",
+    content:
+      "Solaris Software provides no guarantees that any SEO services rendered will achieve any specific or desired outcome.",
+  },
+  {
+    title: "Cancellation Policy",
+    content:
+      "Clients can cancel at any time.  However, any fees paid are non-refundable. Partial project completions are subject to have invoices issued for the work completed up to the point of cancellation.",
+  },
+  {
+    title: "Force Majeure",
+    content:
+      "Solaris Software is not liable for any delays or issues arising from Force Majeure events.  Force Majeure events include but are not limited to natural disasters, pandemics, wars, and acts of terrorism.",
+  },
+  {
+    title: "Indemnification",
+    content:
+      "The client agrees to indemnify and hold harmless Solaris Software from and against any and all claims, costs, damages, losses, liabilities, and expenses (including attorneys' fees and costs) arising out of or in connection with the client's use of the services.",
+  }
+];
