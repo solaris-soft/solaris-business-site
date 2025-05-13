066e2136705b2ac6f96118c330f02fac9376df7c
joshuamURD <jmacmaster9@gmail.com>
2025-05-13 22:05:32 +0800

    Changing fetch strategy

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  prefetch: {
+    defaultStrategy: "hover",
+  },
 });
