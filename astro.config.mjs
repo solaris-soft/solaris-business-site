e956bc8ae89c6845bff7bee5fdde9375cc647c1e
joshuamURD <jmacmaster9@gmail.com>
2025-07-31 13:53:21 +0800

    Updating projects tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
